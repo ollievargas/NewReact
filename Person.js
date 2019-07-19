782dcdc1941a83459976bce5c31c67bff7b386fc
ollievargas <49657659+ollievargas@users.noreply.github.com>
2019-07-18 22:31:47 -0500

    updated Person.js

--- Person.js
+++ Person.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React from 'react'
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}>Im {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
         
-    ) 
+    )
 };
 
-export default person;
+export default person; 
